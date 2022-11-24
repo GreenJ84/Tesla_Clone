98aa6f1ea081321ea6bc8933b6504fc1b2ba3ffd
Jesse Greenough <jesse.greenough84@gmail.com>
2022-11-24 00:10:08 -0800

    Started Home Page layout

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => {
+    return (
+    // Need The Website Header
+    // Need The Rolling selector body
+    <div>HomePage</div>
+    )
+}
+
+export default HomePage
