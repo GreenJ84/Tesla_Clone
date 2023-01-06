7873eefc1394e1bf666247d4c6a7570a6ac65e1b
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-06 05:20:18 -0800

    Set-up app with template routes to build out

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+import { setLogin, setLogout } from './app/User/userSlice';
 
 function App() {
+  const dispatch = useDispatch();
+
+  const auth = () => {
+    if (true) {
+      dispatch(setLogin())
+    } else {
+      dispatch(setLogout())
+    }
+  }
+
   return (
-    <div className="App">
-      Hello, dont break
-    </div>
+    <Routes>
+      {/* Login Page */}
+      <Route />
+      {/* Registration Page */}
+      <Route />
+      {/* Home Page */}
+      <Route />
+      {/* Showcase Page */}
+      <Route />
+      {/* Cart Page */}
+      <Route />
+      {/* Order Page */}
+      <Route />
+      {/* Checkout Page */}
+      <Route/>
+  </Routes>
   );
 }
 
