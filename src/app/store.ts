b30ee467a083b9bd4e7f21cbc2f25fe70fe3f44f
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-06 05:03:41 -0800

    Imported slice reducer for store reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -4,18 +4,19 @@ import { configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
 import { carsData } from "../teslaCarInfo";
+import carReducer from "./Car/carSlice";
+import userReducer from "./User/userSlice";
 
 export const store = configureStore({
   reducer: {
+    user: userReducer,
+    car: carReducer
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
-        // Ignore these action types
         ignoredActions: ["your/action/type"],
-        // Ignore these field paths in all actions
         ignoredActionPaths: ["payload.ref"],
-        // Ignore these paths in the state
         ignoredPaths: [
           ...carsData.map((_, index) => {
             return `car.cars.${index}.ref`;
