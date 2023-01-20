8ba89eece79ce031e4e55b038c696b1604227e6b
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-20 01:54:54 -0800

    Update header structure with template styled divs

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,46 +1,62 @@
-import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
-import { selectCars } from '../app/Store/Car/carSlice';
-import { storeType } from '../app/Store/store';
-import { useCartState } from '../app/Utils/hooks/useCartState';
+/** @format */
 
-interface HeaderType{
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { selectCars } from "../app/Store/Car/carSlice";
+import { storeType } from "../app/Store/store";
+import { useCartState } from "../app/Utils/hooks/useCartState";
+import {
+  Close,
+  Container,
+  Nav,
+  SideMenu,
+  SmallNav,
+} from "../app/Utils/StyledComponents";
 
+interface HeaderType {
+  homeRef?: React.RefObject<HTMLDivElement>;
+  bgColor?: string;
 }
 
 const Header = (props: HeaderType) => {
-    const nav = useNavigate();
-    const dispatch = useDispatch();
+  const nav = useNavigate();
+  const dispatch = useDispatch();
 
-    const { homeRef, bgColor } = props;
-    const user = useSelector((state: storeType) => state.user);
-    const cars = useSelector(selectCars);
-    const [menuStatus, setMenuStatus] = useState(false);
-    const cartData = useCartState();
+  const { homeRef, bgColor } = props;
+  const user = useSelector((state: storeType) => state.user);
+  const cars = useSelector(selectCars);
+  const [menuStatus, setMenuStatus] = useState(false);
+  const cartData = useCartState();
 
-    return (
-        <Container>
-            <Link to="/">
-                <img src="/images/logo.svg" alt="" />
-            </Link>
+  return (
+    <Container>
+      <Link to="/">
+        <img
+          src="/images/logo.svg"
+          alt="Tesla Logo"
+        />
+      </Link>
+      <Nav>
+        {cars.map((car) => (
+          <h1
+            className=""
+            onClick={() => {}}
+            key=""
+          >
+            {car.title}
+          </h1>
+        ))}
+      </Nav>
+      <SideMenu></SideMenu>
+      {/* Right side drop in Nav, under 800px  */}
+      <SmallNav>
+        {/* Close button for Burger Nav */}
+        <Close></Close>
+      </SmallNav>
+    </Container>
+  );
+};
 
-        </Container>
-    )
-}
-
-export default Header
-
-const Container = styled.div`
-    min-height: 60px;
-    position: fixed;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 0 20px;
-    top: 0;
-    left: 0;
-    right: 0;
-    z-index: 1;
-`;
+export default Header;
