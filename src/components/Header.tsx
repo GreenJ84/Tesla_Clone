3cc0a04b234337de84ae1455185195d20265ee8d
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-20 01:24:03 -0800

    Declare initial necessary variables
    Build base Styled Container for component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import { selectCars } from '../app/Store/Car/carSlice';
+import { storeType } from '../app/Store/store';
+import { useCartState } from '../app/Utils/hooks/useCartState';
 
 interface HeaderType{
 
 }
 
 const Header = (props: HeaderType) => {
+    const nav = useNavigate();
+    const dispatch = useDispatch();
+
+    const { homeRef, bgColor } = props;
+    const user = useSelector((state: storeType) => state.user);
+    const cars = useSelector(selectCars);
+    const [menuStatus, setMenuStatus] = useState(false);
+    const cartData = useCartState();
 
     return (
         <Container>
+            <Link to="/">
+                <img src="/images/logo.svg" alt="" />
+            </Link>
 
         </Container>
     )
