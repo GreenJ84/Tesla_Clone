eabdaeb6b0d2f70f2716b44cd9b00c148b03326b
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-20 02:29:37 -0800

    Update Nav styling

--- src/app/Utils/StyledComponents.tsx
+++ src/app/Utils/StyledComponents.tsx
@@ -3,24 +3,35 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  min-height: 60px;
   position: fixed;
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 0 20px;
   top: 0;
   left: 0;
   right: 0;
+  padding: 0 20px;
+  min-height: 60px;
   z-index: 1;
 `;
 
-export const Nav = styled.div``;
+export const Nav = styled.div`
+  display: flex; 
+  justify-content: center;
+  align-items: center;
+  @media (max-width: 770px) {
+    display: none;
+  }
+`;
 
-export const SideMenu = styled.div``;
+export const SideMenu = styled.div`
+
+`;
 
 export const Cart = styled.div``;
 
 export const SmallNav = styled.div``;
 
 export const Close = styled.div``;
+
+export const BurgerIcon = styled.div``;
