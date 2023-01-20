01928c1b921e728caa01740c6b295d2b38daa2f1
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-20 01:54:34 -0800

    Centralize styled components in utils

---
+++ src/app/Utils/StyledComponents.tsx
@@ -0,0 +1,26 @@
+/** @format */
+
+import styled from "styled-components";
+
+export const Container = styled.div`
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 20px;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1;
+`;
+
+export const Nav = styled.div``;
+
+export const SideMenu = styled.div``;
+
+export const Cart = styled.div``;
+
+export const SmallNav = styled.div``;
+
+export const Close = styled.div``;
