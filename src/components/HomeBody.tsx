c0d88e2146cddd571bdcfda7d3a999d6875eb22c
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-22 10:14:15 -0800

    Update HomeBody rendering CarDisplay

--- src/components/HomeBody.tsx
+++ src/components/HomeBody.tsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
-const HomeBody = () => {
+import styled from 'styled-components'
+import CarDisplay from './CarDisplay'
+
+import { carData } from '../teslaCarInfo'
+import { selectCars } from '../app/Store/Car/carSlice';
+
+interface HomeProps{
+    homeRef: React.RefObject<HTMLDivElement>;
+}
+
+const HomeBody = (props: HomeProps) => {
+    const { homeRef } = props;
+    const cars = useSelector(selectCars)
+
     return (
-        <div>HomeBody</div>
+        <Container ref={homeRef}>
+            {cars.map((car: carData) => 
+                <div key={car.id}>
+                <CarDisplay
+                        id={car.id}
+                        title={car.title}
+                        homeRef={homeRef}
+                        backgroundImg={car.backgroundImg}
+                />
+                </div>
+            )}
+        </Container>
     )
 }
 
 export default HomeBody
+
+const Container = styled.div`
+`;
