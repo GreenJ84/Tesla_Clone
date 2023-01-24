60b2b3855daf637e3e580dc7dc54dcb5c2470d36
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-23 21:16:54 -0800

    Remove unnecessary Reference varable

--- src/pages/DisplayPage.tsx
+++ src/pages/DisplayPage.tsx
@@ -1,16 +1,13 @@
 /** @format */
 
-import React, { useRef } from "react";
-
 import Detail from "../components/DisplayPage/Detail";
 import Header from "../components/Layout/Header";
 
 const DisplayPage = () => {
-  const homeRef = useRef<HTMLDivElement>(null);
 
   return (
     <div style={{ position: "relative" }}>
-      <Header homeRef={homeRef} />
+      <Header />
       <Detail />
     </div>
   );
