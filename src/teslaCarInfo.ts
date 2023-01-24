b2b4d354b1e66762ab8a2ba5a9b4451a5fc37dc0
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-23 16:46:35 -0800

    Update flow of Car Data

--- src/teslaCarInfo.ts
+++ src/teslaCarInfo.ts
@@ -13,24 +13,6 @@ export interface carData{
 export const carsData: carData[] = [
    {
       id: 1,
-      title: "Model 3",
-      backgroundImg: "model-s.jpg",
-      description:
-         "Tesla All-Wheel Drive has two independent motors for improved redundancy, each with only one moving part for minimal maintenance and maximum durability. Unlike traditional all-wheel drive systems, they digitally control torque to the front and rear wheels for far better handling and traction control.",
-      price: 41940,
-      inStock: true,
-      highlights: [
-         "Auto Lane Change",
-         "Autopark",
-         "Summon",
-         "Full Self-Driving Computer",
-         "Traffic Light and Stop Sign Control",
-      ],
-      quantity: 1,
-      ref: null,
-   },
-   {
-      id: 2,
       title: "Model Y",
       inStock: true,
       backgroundImg: "model-y.jpg",
@@ -49,13 +31,13 @@ export const carsData: carData[] = [
       ref: null,
    },
    {
-      id: 3,
-      title: "Model X",
-      backgroundImg: "model-x.jpg",
-      price: 108940,
-      inStock: true,
+      id: 2,
+      title: "Model 3",
+      backgroundImg: "model-s.jpg",
       description:
-         "Tesla designed Model X from the ground-up as an electric vehicle — each component, including batteries, motors and exterior aerodynamics are optimized to benefit from one another and create one of the most efficient, yet unbelievably powerful vehicles ever built.",
+         "Tesla All-Wheel Drive has two independent motors for improved redundancy, each with only one moving part for minimal maintenance and maximum durability. Unlike traditional all-wheel drive systems, they digitally control torque to the front and rear wheels for far better handling and traction control.",
+      price: 41940,
+      inStock: true,
       highlights: [
          "Auto Lane Change",
          "Autopark",
@@ -67,7 +49,7 @@ export const carsData: carData[] = [
       ref: null,
    },
    {
-      id: 4,
+      id: 3,
       title: "Model S",
 
       description:
@@ -78,6 +60,24 @@ export const carsData: carData[] = [
 
       highlights: [
          "Navigate on Autopilot",
+         "Auto Lane Change",
+         "Autopark",
+         "Summon",
+         "Full Self-Driving Computer",
+         "Traffic Light and Stop Sign Control",
+      ],
+      quantity: 1,
+      ref: null,
+   },
+   {
+      id: 4,
+      title: "Model X",
+      backgroundImg: "model-x.jpg",
+      price: 108940,
+      inStock: true,
+      description:
+         "Tesla designed Model X from the ground-up as an electric vehicle — each component, including batteries, motors and exterior aerodynamics are optimized to benefit from one another and create one of the most efficient, yet unbelievably powerful vehicles ever built.",
+      highlights: [
          "Auto Lane Change",
          "Autopark",
          "Summon",
