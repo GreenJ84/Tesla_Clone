45f5ea4cc1cb0019c9220b8b99aa6047f2100210
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-23 16:47:06 -0800

    Add new stlyed componented
    Update Tailwinds styling and responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,6 +16,7 @@ import { useCartState } from "../app/Utils/hooks/useCartState";
 import {
   Close,
   Container,
+  Cover,
   Nav,
   SideMenu,
   SmallNav,
@@ -50,7 +51,7 @@ const Header = (props: HeaderType) => {
       {/* Tesla Logo Display */}
       <Link to="/">
         <img
-          className="relative top-2 left-2 w-36 h-10 xl:w-46 xl:h-16 2xl:top-5 2xl:left-5 2xl:w-52 2xl:h-20"
+          className="relative top-2 left-2 w-36 h-6 xl:w-46 2xl:top-5 2xl:left-5 2xl:w-52"
           src="/images/logo.svg"
           alt="Tesla Logo"
         />
@@ -80,39 +81,44 @@ const Header = (props: HeaderType) => {
       <SideMenu>
         {user.isLoggedIn
           ?
-            <h1
-              className="text-black text-lg bg-transparent rounded-lg px-2.5 py-1.5 hover:text-black hover:bg-gray-500 cursor hover:bg-opacity-30 transition-colors mr-5 hover:shadow-2xl"
-              onClick={() => {
-                dispatch(setLogout());
-                window.location.reload();
-              }}
-            >
-              Logout
-            </h1>
+            <>
+              <Link to="/cart"
+                className="flex relative items-center"
+              >
+                <FaOpencart className="relative top-3 md:h-10 md:w-10 h-8 w-8"/>
+                <p
+                  className="absolute -top-0.5 right-1 md:-top-1 md:right-2 flex items-center justify-center bg-red-600 text-white h-5 w-5 md:h-6 md:w-6 text-sm font-bold rounded-full">
+                  {cartData.length}
+                </p>
+              </Link>
+              <h1
+                className="text-black text-lg bg-transparent rounded-lg px-2.5 py-1.5 hover:text-black hover:bg-gray-500 cursor hover:bg-opacity-30 transition-colors mr-5 hover:shadow-2xl"
+                onClick={() => {
+                  dispatch(setLogout());
+                  window.location.reload();
+                }}
+              >
+                Logout
+              </h1>
+            </>
           :
             <h1
-            className="text-black text-lg bg-transparent rounded-lg px-2.5 py-1.5 hover:text-black hover:bg-gray-500 cursor hover:bg-opacity-30 transition-colors mr-5 hover:shadow-2xl"
+            className="hidden xl:inline-block text-black text-lg bg-transparent rounded-lg px-2.5 py-1.5 hover:text-black hover:bg-gray-500 cursor hover:bg-opacity-30 transition-colors mr-5 hover:shadow-2xl"
               onClick={() => nav('/login')}
             >
-              Sign In
+              Account
             </h1>
         }
-          <Link to="/cart"
-            className="flex relative items-center"
-          >
-            <FaOpencart className="relative top-3 md:h-10 md:w-10 h-8 w-8"/>
-            <p
-              className="absolute -top-0.5 right-1 md:-top-1 md:right-2 flex items-center justify-center bg-red-600 text-white h-5 w-5 md:h-6 md:w-6 text-sm font-bold rounded-full">
-              {cartData.length}
-            </p>
-          </Link>
 
-          <Bars4Icon
-            style={{ "cursor": "pointer" }}
-            className="md:hidden block h-8 w-8"
+          <p
+            style={{ "cursor": "pointer", "borderRadius": "5px" }}
+            className="xl:bg-transparent bg-slate-300 px-6 py-2 text-lg text-black font-medium"
             onClick={() => setMenuStatus(true)}
-          />
+          >
+            Menu
+          </p>
       </SideMenu>
+      <Cover show={menuStatus} />
       {/* Right side drop in Nav, under 800px  */}
       <SmallNav show={menuStatus}>
         {/* Close button for Burger Nav */}
