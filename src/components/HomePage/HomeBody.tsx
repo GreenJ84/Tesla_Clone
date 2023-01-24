f3ad4949d3e05ec069fcafe348976b63c33580ca
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-23 19:30:49 -0800

    Move HomeBody into HomePage components folder

---
+++ src/components/HomePage/HomeBody.tsx
@@ -0,0 +1,43 @@
+/** @format */
+
+import React from "react";
+import { useSelector } from "react-redux";
+
+import styled from "styled-components";
+import Display from "./Display";
+
+import { carData } from "../../teslaCarInfo";
+import { selectCars } from "../../app/Store/Car/carSlice";
+
+interface HomeProps {
+  homeRef: React.RefObject<HTMLDivElement>;
+}
+
+const HomeBody = (props: HomeProps) => {
+  const { homeRef } = props;
+  const cars = useSelector(selectCars);
+
+  return (
+    <Container ref={homeRef}>
+      {cars.map((car: carData) => (
+        <div key={car.id}>
+          <Display
+            id={car.id}
+            title={car.title}
+            homeRef={homeRef}
+            backgroundImg={car.backgroundImg}
+          />
+        </div>
+      ))}
+    </Container>
+  );
+};
+
+export default HomeBody;
+
+const Container = styled.div`
+  overflow-y: auto;
+  text-align: center;
+  height: 100vh;
+  scroll-snap-type: y mandatory;
+`;
