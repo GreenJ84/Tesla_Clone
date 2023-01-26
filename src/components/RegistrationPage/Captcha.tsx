9af382b7b3db8fa81abf6fa7f56ccdb64542c685
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-26 14:59:45 -0800

    Create component to render and refresh captcha

--- src/components/RegistrationPage/Captcha.tsx
+++ src/components/RegistrationPage/Captcha.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { ArrowPathIcon } from '@heroicons/react/24/solid';
+import React, { useState } from 'react'
+import styled from 'styled-components';
+import { useCaptcha } from '../../app/Utils/hooks/useCaptcha';
 
-const Captcha = () => {
+interface captchaProps{
+  cap: [cap: string, setCap: Function]
+}
+
+const Captcha = (props: captchaProps) => {
+  const [cap, setCap] = props.cap;
+
+
   return (
-    <div>Captcha</div>
+    <Container >
+      <p>{ cap }</p>
+      <ArrowPathIcon
+        className="absolute cursor right-2 top-2 w-6 h-6"
+        onClick={() => setCap()}
+      />
+    </Container>
   )
 }
 
 export default Captcha
+
+const Container = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 430px;
+  background-color: rgba(245,245,245);
+  height: 110px;
+  border-radius: 5px;
+  margin-bottom: 50px;
+  p{
+    position: relative;
+    top: 14px;
+    font-size: 28px;
+    letter-spacing: 5px;
+  }
+`;
