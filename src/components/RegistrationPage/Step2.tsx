37792916c8a4d6baaf25f9ca5ffc8e203f236e99
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-26 01:18:28 -0800

    Add functionality to Step2

--- src/components/RegistrationPage/Step2.tsx
+++ src/components/RegistrationPage/Step2.tsx
@@ -1,18 +1,19 @@
 import React, { useState } from 'react'
 import Show from '../Layout/Show'
+import { InformationCircleIcon } from '@heroicons/react/24/solid'
 
 interface Step2 {
-    email: string
-    setEmail: Function
-    password: string
-    setPassword: Function
+    email: [string, Function]
+    password: [string, Function]
 }
 
 const Step2 = (props: Step2) => {
-    const { email, setEmail, password, setPassword } = props;
+    const [email, setEmail] = props.email;
+    const [password, setPassword] = props.password;
     const [showPass, setShowPass] = useState(false);
     const [showCon, setShowCon] = useState(false);
     const [conPass, setConPass] = useState("");
+    const confirmMatch = !conPass || conPass === password
 
     const showPassHandler = () => {
         showPass ?
@@ -22,6 +23,8 @@ const Step2 = (props: Step2) => {
         showCon ?
             setShowCon(false) : setShowCon (true)
     }
+
+
     return (
         <>
             <label htmlFor="" inputMode='email'>Email</label>
@@ -29,23 +32,35 @@ const Step2 = (props: Step2) => {
                 type="text"
                 value={email}
                 onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.currentTarget.value)}
+                required
             />
 
-            <label htmlFor="">Password</label>
+            <label htmlFor="">
+                Password
+                <span><InformationCircleIcon /></span>
+                <span>Password must be at least 8 characters and include at least 1 number and 1 letter</span>
+            </label>
             <input 
                 type={showPass ? "text" : "password"}
                 value={password}
                 onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.currentTarget.value)}
+                required
             />
-            <Show state={ showPass } set={ setShowPass }/>
+            <Show state={ showPass } set={ showPassHandler }/>
 
             <label htmlFor="">Confirm Password</label>
             <input 
+                className={confirmMatch ? 
+                    ""
+                :
+                    ""
+                }
                 type={showCon ? "text" : "password"} 
                 value={conPass}
                 onChange={(e: React.ChangeEvent<HTMLInputElement>) => setConPass(e.currentTarget.value)}
+                required
             />
-            <Show state={ showCon } set={ setShowCon }/>
+            <Show state={ showCon } set={ showConHandler }/>
         </>
   )
 }
