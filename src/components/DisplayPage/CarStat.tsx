3db9732ee960a8a4a0727d79d783d643ede8cae8
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-25 16:30:36 -0800

    Update Car Stat styling

--- src/components/DisplayPage/CarStat.tsx
+++ src/components/DisplayPage/CarStat.tsx
@@ -12,8 +12,12 @@ const CarStat = (props: CarStats) => {
     const { value, description } = props;
     return (
     <Container>
-            <h1>{ value }</h1>
-            <h2>{ description }</h2>
+            <h1>
+                {value}
+            </h1>
+            <h2>
+                {description}
+            </h2>
     </Container>
     );
 };
@@ -21,5 +25,19 @@ const CarStat = (props: CarStats) => {
 export default CarStat;
 
 const Container = styled.div`
-
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    h1{
+        color: white;
+        font-size: 36px;
+        font-weight: 600;
+        text-shadow: 0px 0px 8px rgba(255,255,255, 0.4);
+    }
+    h2{
+        margin-top: 6px;
+        color: lightgrey;
+        font-size: 18px;
+    }
 `;
