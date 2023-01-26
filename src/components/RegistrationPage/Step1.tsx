0a162c69d42db4f72c3a3e09987955eca2acdf20
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-26 01:33:05 -0800

    Add functionality to Step 1

--- src/components/RegistrationPage/Step1.tsx
+++ src/components/RegistrationPage/Step1.tsx
@@ -1,23 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Captcha from './Captcha'
 
-const Step1 = () => {
+interface Step1 {
+    firstName: [string, Function]
+    lastName: [string, Function]
+    username: [string, Function]
+}
+
+const Step1 = (props:Step1) => {
+    const [fN, setFN] = props.firstName;
+    const [lN, setLN] = props.lastName;
+    const [un, setUn] = props.username;
+    const [capcha, setCapcha] = useState("");
   return (
     <>
-        <label htmlFor="">
+        <label htmlFor="first-name">
             First Name
         </label>
-        <input name="" type="text" />
-        <label htmlFor="">
+        <input 
+            name="first-name" 
+            type="text"
+            value={fN}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                setFN(e.currentTarget.value)
+            }}
+            required
+        />
+        <label htmlFor="last-name">
             Last Name
         </label>
-        <input name="" type="text" />
-        <div>
-            
-        </div>
-        <label htmlFor="">
+        <input 
+            name="last-name" 
+            type="text"
+            value={lN}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                setLN(e.currentTarget.value)
+            }}
+            required
+        />
+        <Captcha />
+        <label htmlFor="CAPTCHA">
             Enter the characters in the image
         </label>
-        <input name="" type="text" />
+        <input 
+            name="CAPTCHA" 
+            type="text"
+            value={""}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+
+            }}
+        />
         <p>
             By continuing, I understand and agree to Tesla's <span>Privacy Notice</span> and <span>Terms of Use</span> for creating a Tesla Account
         </p>
