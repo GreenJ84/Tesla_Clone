cbbb894ebe465b40b93ff506797f0bb0795fd916
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-26 00:27:50 -0800

    Create Registration Step 1 skeleton

---
+++ src/components/RegistrationPage/Step1.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Step1 = () => {
+  return (
+    <>
+        <label htmlFor="">
+            First Name
+        </label>
+        <input name="" type="text" />
+        <label htmlFor="">
+            Last Name
+        </label>
+        <input name="" type="text" />
+        <div>
+            
+        </div>
+        <label htmlFor="">
+            Enter the characters in the image
+        </label>
+        <input name="" type="text" />
+        <p>
+            By continuing, I understand and agree to Tesla's <span>Privacy Notice</span> and <span>Terms of Use</span> for creating a Tesla Account
+        </p>
+        <button>Next</button>
+    </>
+  )
+}
+
+export default Step1
