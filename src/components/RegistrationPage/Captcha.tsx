18e79151866b8dda95996312f5d416d357c2a1c5
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-27 00:34:40 -0800

    Remove unused code and imports
    Organize and format code

--- src/components/RegistrationPage/Captcha.tsx
+++ src/components/RegistrationPage/Captcha.tsx
@@ -1,28 +1,28 @@
-import { ArrowPathIcon } from '@heroicons/react/24/solid';
-import React, { useState } from 'react'
-import styled from 'styled-components';
-import { useCaptcha } from '../../app/Utils/hooks/useCaptcha';
+/** @format */
 
-interface captchaProps{
-  cap: [cap: string, setCap: Function]
+import styled from "styled-components";
+
+import { ArrowPathIcon } from "@heroicons/react/24/solid";
+
+interface captchaProps {
+  cap: [cap: string, setCap: Function];
 }
 
 const Captcha = (props: captchaProps) => {
   const [cap, setCap] = props.cap;
 
-
   return (
-    <Container >
-      <p>{ cap }</p>
+    <Container>
+      <p>{cap}</p>
       <ArrowPathIcon
         className="absolute cursor right-2 top-2 w-6 h-6"
         onClick={() => setCap()}
       />
     </Container>
-  )
-}
+  );
+};
 
-export default Captcha
+export default Captcha;
 
 const Container = styled.div`
   position: relative;
@@ -30,12 +30,12 @@ const Container = styled.div`
   align-items: center;
   justify-content: center;
   width: 430px;
-  background-color: rgba(240,240,240);
+  background-color: rgba(240, 240, 240);
   height: 95px;
   border-radius: 10px;
   margin-top: 5px;
   margin-bottom: 24px;
-  p{
+  p {
     position: relative;
     top: 14px;
     font-size: 28px;
