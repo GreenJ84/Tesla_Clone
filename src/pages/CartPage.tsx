509b2c1f2dfacf1236a10ac2415c7e5673eb4c48
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-27 01:25:59 -0800

    Create skeleton Cart Page component

---
+++ src/pages/CartPage.tsx
@@ -0,0 +1,50 @@
+
+import { useDispatch } from 'react-redux';
+import styled from 'styled-components';
+
+import OrderSummary from '../components/CartPage/OrderSummary';
+import ShopHeader from '../components/Layout/ShopHeader';
+import SmallFooter from '../components/Layout/SmallFooter';
+
+import { useCartState } from '../app/Utils/hooks/useCartState';
+import CartItem from '../components/CartPage/CartItem';
+
+const CartPage = () => {
+    
+    const _products = useCartState();
+    const dispatch = useDispatch();
+
+
+    return (
+    <> 
+        <ShopHeader />
+        <h1> Cart </h1>
+        <Container>
+            <Cart>
+                <>
+                {_products.map((product) => {
+                    <span key={product.id}>
+                        <CartItem product={product} />
+                    </span>
+                })}
+                </>
+            </Cart>
+            <OrderSummary />
+        </Container>
+        <SmallFooter />
+    </> 
+  )
+}
+
+export default CartPage;
+
+const Container = styled.div`
+    display: flex;
+@media screen and (max-width: 1100px){
+    flex-direction: column;
+}
+`;
+
+const Cart = styled.div`
+
+`;
