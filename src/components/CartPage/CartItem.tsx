2068ebf782192c90b598d187b78c0c351837cf84
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-27 12:08:27 -0800

    Eslint formatting

--- src/components/CartPage/CartItem.tsx
+++ src/components/CartPage/CartItem.tsx
@@ -1,30 +1,37 @@
-import React from 'react'
-import styled from 'styled-components';
-import { carData } from '../../teslaCarInfo';
+/** @format */
 
-interface CartProps{
-    product: carData
+import styled from "styled-components";
+import { carData } from "../../teslaCarInfo";
+
+interface CartProps {
+  product: carData;
 }
 
 const CartItem = (props: CartProps) => {
-    const { product } = props;
+  const { product } = props;
   return (
     <Container>
-        <img src={product.backgroundImg} alt={product.title} />
+      <img
+        src={product.backgroundImg}
+        alt={product.title}
+      />
+      <div>
+        <p>{product.title}</p>
         <div>
-            <p>{ product.title}</p>
-            <div>
-                <input type="number" value={ product.quantity} min={0} max={3} />
-                <p>Remove</p>
-            </div>
-              <p>${ product.price }.00</p>
+          <input
+            type="number"
+            value={product.quantity}
+            min={0}
+            max={3}
+          />
+          <p>Remove</p>
         </div>
+        <p>${product.price}.00</p>
+      </div>
     </Container>
-  )
-}
+  );
+};
 
-export default CartItem
+export default CartItem;
 
-const Container = styled.li`
-
-`;
+const Container = styled.li``;
