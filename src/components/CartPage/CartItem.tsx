d54970b36d4201c4cb6d9eafd281e058c1626a28
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-29 16:11:32 -0800

    Styled cart item, update img url

--- src/components/CartPage/CartItem.tsx
+++ src/components/CartPage/CartItem.tsx
@@ -12,26 +12,67 @@ const CartItem = (props: CartProps) => {
   return (
     <Container>
       <img
-        src={product.backgroundImg}
+        src={`/images/${product.backgroundImg}`}
         alt={product.title}
       />
       <div>
         <p>{product.title}</p>
-        <div>
-          <input
+        <div className="flex">
+          <p>
+            Quantity: <input
             type="number"
             value={product.quantity}
             min={0}
             max={3}
           />
+          </p>
           <p>Remove</p>
         </div>
-        <p>${product.price}.00</p>
       </div>
+      <p>${product.price}.00</p>
     </Container>
   );
 };
 
 export default CartItem;
 
-const Container = styled.li``;
+const Container = styled.li`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 170px;
+  >img{
+    height 100px;
+    width: 100px;
+  }
+  >div{
+    position: relative;
+    left: -80px;
+    top: -20px;
+    >p{
+      font-size: 22px;
+      font-weight: 500;
+      letter-spacing: 1.2px;
+      margin-bottom: 5px;
+    }
+    >div{
+      font-size: 18px;
+      >p:nth-of-type(2){
+        padding-bottom: .5px;
+        color: rgba(80, 80, 80);
+        border-bottom: 1.5px solid black;
+      }
+      >p:nth-of-type(2):hover{
+        padding-bottom: 0px;
+        color: rgba(0, 0, 0);
+        border-bottom: 2px solid black;
+      }
+    }
+  }
+  >p{
+    position: relative;
+    top: -38px;
+    font-size: 22px;
+    font-weight: 500;
+  }
+`;
