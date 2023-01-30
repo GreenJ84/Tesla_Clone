ba73ddaf203e51f6be8e20bfe53be03c4818b28e
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-29 23:25:34 -0800

    Create 2 step order page

---
+++ src/pages/OrderPage.tsx
@@ -0,0 +1,54 @@
+/** @format */
+
+import React, { useState } from "react";
+import Order1 from "../components/OrderPage/Order1";
+import Order2 from "../components/OrderPage/Order2";
+
+const OrderPage = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [address, setAddress] = useState("");
+  const [address2, setAddress2] = useState("");
+  const [zip, setZip] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const [secStep, setSecStep] = useState(false)
+
+  return (
+    <>
+      {secStep ? 
+        <p onClick={() => setSecStep(false)}>Back</p>
+      :
+        ""
+      }
+      <p>Step { secStep ? '2' : "1" } of 2</p>
+      {secStep ?
+        <Order1
+          fn={[firstName, setFirstName]}
+          ln={[lastName, setLastName]}
+          add={[address, setAddress]}
+          add2={[address2, setAddress2]}
+          zip={[zip, setZip]}
+          city={[city, setCity]}
+          state={[state, setState]}
+          phone={[phone, setPhone]}
+        />
+        :
+        <Order2
+        fn={firstName}
+        ln={lastName}
+        add={address}
+        add2={address2}
+        zip={zip}
+        city={city}
+        state={state}
+        phone={phone}
+        />
+      }
+    </>
+  );
+};
+
+export default OrderPage;
