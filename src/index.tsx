69e400bfca9150c19b5e1e69ab3c645debd8eeb3
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-31 23:36:00 -0800

    Update firebase config

--- src/index.tsx
+++ src/index.tsx
@@ -15,17 +15,18 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider, FacebookAuthProvider, TwitterAuthProvider, GithubAuthProvider } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
+
 const firebaseConfig = {
-  apiKey: process.env!.API_KEY,
+  apiKey: "AIzaSyCYjBCaFh6cbvKYvu6s4sGJl3KLk4NVArw",
   authDomain: "tesla-gclone.firebaseapp.com",
   projectId: "tesla-gclone",
   storageBucket: "tesla-gclone.appspot.com",
-  messagingSenderId: process.env!.MSG_ID,
-  appId: process.env!.APP_ID,
+  messagingSenderId: "248739858073",
+  appId: "1:248739858073:web:12b0ab828326283f4cda08",
 };
 export const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-export const auth = getAuth();
 export const googleProvider = new GoogleAuthProvider();
 export const facebookProvider = new TwitterAuthProvider();
 export const twitterProvider = new FacebookAuthProvider();
