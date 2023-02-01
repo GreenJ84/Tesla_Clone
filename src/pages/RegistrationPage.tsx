1624aa0381806aeba9b56232d66331ee2e37248e
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-31 23:36:20 -0800

    Update Reg components props
    Add state for confirmed password and errors
    
    Add firebase user registration

--- src/pages/RegistrationPage.tsx
+++ src/pages/RegistrationPage.tsx
@@ -2,19 +2,62 @@
 
 import { useState } from "react";
 import styled from "styled-components";
+import { getAuth, createUserWithEmailAndPassword, updateProfile, sendEmailVerification } from "firebase/auth";
 
 import LogRegHeader from "../components/Layout/MinimalHeader";
 import LogRegFooter from "../components/Layout/SmallFooter";
 import Reg1 from "../components/RegistrationPage/Reg1";
 import Reg2 from "../components/RegistrationPage/Reg2";
+import { useNavigate } from "react-router-dom";
 
 const RegistrationPage = () => {
+  const nav = useNavigate();
+
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confPassword, setConfPassword] = useState("");
 
   const [secStep, setSecStep] = useState(false);
+  const [error, setError] = useState("");
+
+  const register = async () => {
+    if (password !== confPassword) {
+      setError("Passwords are not a match");
+      return;
+    }
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(async () => {
+        // firebase authenticated and signed in
+        if (!auth.currentUser) return;
+        sendEmailVerification(auth.currentUser);
+        // eslint-disable-next-line no-restricted-globals
+        await confirm(`A verification email has been sent to ${auth.currentUser.email}`);
+
+        updateProfile(auth.currentUser, {
+          displayName: `${firstName}
+            ${lastName ? ` ${lastName}` : ""}`
+        })
+          .then(() => {
+            console.log('Profile updated');
+          })
+          .catch(err =>
+            console.log("User update error has occured\n", err
+            ));
+        // redirect for app sign
+        nav('/login');
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message.split(": ")[1];
+        
+        console.log("A registration error has occured\n", `Error ${errorCode}: ${errorMessage}`)
+        setError(errorMessage);
+        setSecStep(false);
+      });
+  }
 
   return (
     <>
@@ -23,6 +66,11 @@ const RegistrationPage = () => {
         <p> Step {!secStep ? "1" : "2"} of 2</p>
         <h1>Create Account</h1>
         <form>
+          {error ?
+            <span>{ error }</span>
+          : 
+            ""
+          }
           {!secStep ? (
             <Reg1
               firstName={[firstName, setFirstName]}
@@ -33,6 +81,8 @@ const RegistrationPage = () => {
             <Reg2
               email={[email, setEmail]}
               password={[password, setPassword]}
+              confPassword={[confPassword, setConfPassword]}
+              register={register}
             />
           )}
         </form>
