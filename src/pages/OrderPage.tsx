dc1ee7fe25927215e61eb7d090b58c1c4927ed2d
Jesse Greenough <jesse.greenough84@gmail.com>
2023-02-05 00:29:18 -0800

    Update Page structure and imports

--- src/pages/OrderPage.tsx
+++ src/pages/OrderPage.tsx
@@ -2,7 +2,9 @@
 
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 import { useUserData } from "../app/Store/User/userSlice";
+import Header from "../components/Layout/Header";
 import Order1 from "../components/OrderPage/Order1";
 import Order2 from "../components/OrderPage/Order2";
 
@@ -30,38 +32,43 @@ const OrderPage = () => {
 
   return (
     <>
-      {secStep ? 
-        <p onClick={() => setSecStep(false)}>Back</p>
-      :
-        ""
-      }
-      <p>Step { secStep ? '2' : "1" } of 2</p>
-      {secStep ?
-        <Order1
-          fn={[firstName, setFirstName]}
-          ln={[lastName, setLastName]}
-          add={[address, setAddress]}
-          add2={[address2, setAddress2]}
-          zip={[zip, setZip]}
-          city={[city, setCity]}
-          state={[state, setState]}
-          phone={[phone, setPhone]}
-          setStep={() => setSecStep(true)}
-        />
+      <Header />
+      <Container>
+        {secStep ? 
+          <p onClick={() => setSecStep(false)}>Back</p>
         :
-        <Order2
-        fn={firstName}
-        ln={lastName}
-        add={address}
-        add2={address2}
-        zip={zip}
-        city={city}
-        state={state}
-        phone={phone}
-        />
-      }
+          ""
+        }
+        <p>Step { secStep ? '2' : "1" } of 2</p>
+        {!secStep ?
+          <Order1
+            fn={[firstName, setFirstName]}
+            ln={[lastName, setLastName]}
+            add={[address, setAddress]}
+            add2={[address2, setAddress2]}
+            zip={[zip, setZip]}
+            city={[city, setCity]}
+            state={[state, setState]}
+            phone={[phone, setPhone]}
+            setStep={() => setSecStep(true)}
+          />
+          :
+          <Order2
+          fn={firstName}
+          ln={lastName}
+          add={address}
+          add2={address2}
+          zip={zip}
+          city={city}
+          state={state}
+          phone={phone}
+          />
+        }
+      </Container>
     </>
   );
 };
 
 export default OrderPage;
+
+const Container = styled.div``;
