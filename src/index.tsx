c273b024673d6dccbecde86afec631fc8288f31a
Jesse Greenough <jesse.greenough84@gmail.com>
2023-02-05 14:20:05 -0800

    Wrap app with PersistGate

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import App from "./App";
-import { store } from "./app/Store/store";
+import { persistor, store } from "./app/Store/store";
 
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
@@ -42,7 +43,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
