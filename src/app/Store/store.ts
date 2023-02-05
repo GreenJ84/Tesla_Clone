fa4901ea1a7837fecbad0cfad4c8f76953bc4be5
Jesse Greenough <jesse.greenough84@gmail.com>
2023-02-05 14:19:53 -0800

    Persist store state and export persistor

--- src/app/Store/store.ts
+++ src/app/Store/store.ts
@@ -1,20 +1,32 @@
 /** @format */
 
 import { configureStore } from "@reduxjs/toolkit";
+import { FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE } from 'redux-persist';
+import autoMergeLevel2 from "redux-persist/es/stateReconciler/autoMergeLevel2";
+import storage from "redux-persist/lib/storage";
 
 import { carsData } from "../../teslaCarInfo";
-import carReducer from "./Car/carSlice";
-import userReducer from "./User/userSlice";
+import carReducer, { CarState } from "./Car/carSlice";
+import userReducer, { UserState } from "./User/userSlice";
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  stateReconciler: autoMergeLevel2
+}
+
+const persistedUserReducer = persistReducer<UserState>(persistConfig, userReducer)
+const persistedCarReducer = persistReducer<CarState>(persistConfig, carReducer)
 
 export const store = configureStore({
   reducer: {
-    user: userReducer,
-    car: carReducer,
+    user: persistedUserReducer,
+    car: persistedCarReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
-        ignoredActions: ["user/setLogin"],
+        ignoredActions: ["user/setLogin", FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
         ignoredActionPaths: ["payload.ref", "user.user"],
         ignoredPaths: [
           ...carsData.map((_, index) => {
@@ -22,9 +34,10 @@ export const store = configureStore({
           }),
         "user.user"],
       },
-      immutableCheck: true,
+      immutableCheck: false,
     }),
 });
 
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
