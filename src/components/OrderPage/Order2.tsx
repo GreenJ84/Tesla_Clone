73cd88e94030f122fca7543939ee94f2b01057d3
Jesse Greenough <jesse.greenough84@gmail.com>
2023-02-05 19:45:11 -0800

    Update styled components implemented
    Update flow of main component
    
    Refactor code for create new styled components for similar code

--- src/components/OrderPage/Order2.tsx
+++ src/components/OrderPage/Order2.tsx
@@ -7,8 +7,10 @@ import styled from "styled-components";
 import { setTotal } from "../../app/Store/Car/carSlice";
 import { useAppSelector } from "../../app/Utils/hooks/useAppSelector";
 import { useCartState } from "../../app/Utils/hooks/useCartState";
-import { Button } from "../../app/Utils/StyledComponents/DisplayComponents";
+import { Body } from "../../pages/CartPage";
+import CartItem from "../CartPage/CartItem";
 import CardModal from "./CardModal";
+import FinalOrder from "./FinalOrder";
 
 interface order2Props{
   fn: string
@@ -42,97 +44,195 @@ const Order2 = (props: order2Props) => {
   }
 
   return (
-    <Container>
+    <Order2Body>
       <h1>Review and Pay</h1>
-      <p>Order Summary({_products.length} items)</p>
-      <ul>
-      {_products.map((product) =>
-        <ListItem key={product.id}>
-          <img
-            src={`/images/${product.backgroundImg}`}
-            alt={product.title}
-          />
+      <p>Order Summary ({_products.length} items)</p>
+      <Container>
+        <ul>
+        {_products.map((product) =>
+          <li key={product.id}>
+            <CartItem product={product} />
+          </li>
+        )}
+        </ul>
+
+        <Address>
           <div>
-            <p>{product.title}</p>
-            <p>
-              Quantity: { product.quantity }
-            </p>
+            <h3>Shipping Address</h3>
+            <button>
+              Edit
+            </button>
           </div>
-          <p>${product.price}.00</p>
-        </ListItem>
-      )}
-      </ul>
+          <p>Jesse Greenough{fn} { ln }</p>
+          <p>19240 Aurora{ add }</p>
+          {add2 ? <p>{add2}</p> : <p>2-310</p>}
+          <p>Shoreline{city}, WA 98133{state} {zip}</p>
+          <p>425-599-3275{ phone }</p>
+        </Address>
 
-      <div>
-        <h1>Shipping Address</h1>
-        <p>{fn} { ln }</p>
-        <p>{ add }</p>
-        {add2 ? <p>{add2}</p> : ""}
-        <p>{city}, {state} {zip}</p>
-        <p>{ phone }</p>
-      </div>
-
-      <div>
-        <h1>Billing Address</h1>
-        <p>{fn} { ln }</p>
-        <p>{ add }</p>
-        {add2 ? <p>{add2}</p> : ""}
-        <p>{city}, {state} {zip}</p>
-      </div>
-
-      <p> Add Promo Code </p>
-
-      <div>
+        <Address>
         <div>
-          <p>Subtotal</p>
-          <p>Shipping</p>
-          <p>Sales Tax</p>
-          <h2>Total Due</h2>
-        </div>
-        <div>
-          <p> { subTotal }</p>
-          <p> Free </p>
-          <p> { getTax(subTotal)} </p>
-          <h2>{ subTotal + getTax(subTotal) }</h2>
-        </div>
-      </div>
-      <button
-        onClick={() => toggleCardModal}
-      >
-        Card
-      </button>
-      {cardModal ? 
-        <CardModal setCard={() => setCard(true) } />
-      :
-        ""}
-      <p>Add Gift Card </p>
-      <p> By continuing, I understand and agree to the General Terms and Conditions of Online Car and Accessories Sales, <span>Terms of Use</span> and <span>Privacy Notice</span>.</p>
-      {card ?
+            <h3>Billing Address</h3>
+            <button>
+              Edit
+            </button>
+          </div>
+          <p>Jesse Greenough{fn} { ln }</p>
+          <p>19240 Aurora{ add }</p>
+          {add2 ? <p>{add2}</p> : <p>2-310</p>}
+          <p>Shoreline{city}, WA 98133{state} {zip}</p>
+        </Address>
+
+        <p>
+          Add Promo Code
+        </p>
+
+        <FinalOrder subTot={subTotal} />
         <Button
-          onClick={() => {
-            dispatch(setTotal(subTotal + getTax(subTotal)));
-            nav('/confirmation');
-          }}
+          
+          onClick={() => toggleCardModal}
         >
-          Place Order
+          Card
         </Button>
-      :
-        <Button
-          disabled
-        >
-          Place Order
-        </Button>
-      }
-    </Container>
+        {cardModal ? 
+          <CardModal setCard={() => setCard(true) } />
+        :
+          ""}
+        <p>
+          Add Gift Card
+        </p>
+        <p> By continuing, I understand and agree to the General Terms and Conditions of Online Accessories Sales, <span>Terms of Use</span> and <span>Privacy Notice</span>.</p>
+        {card ?
+          <Button
+            className="px-8"
+            onClick={() => {
+              dispatch(setTotal(subTotal + getTax(subTotal)));
+              nav('/confirmation');
+            }}
+          >
+            Place Order
+          </Button>
+        :
+          <Button
+            className="px-4"
+            disabled
+          >
+            Place Order
+          </Button>
+          }
+        </Container>
+    </Order2Body>
   );
 };
 
 export default Order2;
 
-const Container = styled.div`
-  
+const Order2Body = styled(Body)`
+  >h1{
+    position: relative;
+    bottom: 16px;
+    font-size: 60px;
+    font-weight: 500;
+  }
+  >p{
+    font-size: 19px;
+    letter-spacing: 1.05px;
+  }
 `;
 
-const ListItem = styled.li`
+const Container = styled.div`
+  display: flex;
+  >ul{
+    padding: 50px 0;
+    margin-bottom: 70px;
+    border-bottom: 1px solid grey;
+  }
+  >p:first-of-type{
+    font-size: 19px;
+    font-weight: 500;
+    margin-bottom: 34px;
+    color: rgba(30,30,30)
+  }
+  >p:nth-of-type(2){
+    font-size: 18px;
+    font-weight: 350;
+    letter-spacing: 1.04px;
+    margin-bottom: 32px;
+  }
+  >p:nth-of-type(3){
+    font-size: 16px;
+    font-weight: 500;
+    margin-bottom: 30px;
+    letter-spacing: 1.04px;
+    word-spacing: 1.2px;
+    color: rgba(90, 90, 90);
+    >span{
+      color: inherit;
+      border-bottom: 1.5px solid black;
+      :hover{
+        border-bottom: 2px solid black;
+        color: black;
+      }
+    }
+    
+  }
+  @media screen and (min-width: 1280px) {
 
+  }
+  @media screen and (max-width: 1280px) {
+    flex-direction: column;
+  }
+`;
+
+const Address = styled.div`
+  letter-spacing: 1.1px;
+  line-height: 28px;
+  margin-bottom: 27px;
+  >div{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    >h3{
+      font-size: 20px;
+      font-weight: 350;
+      color: rgba(90, 90, 90);
+      margin-bottom: 6px;
+    }
+    >button{
+      position: relative;
+      right: 26px;
+      font-size: 20px;
+      color: rgba(80, 80, 80);
+      border-bottom: 1.5px solid rgba(80, 80, 80);
+      line-height: 24px;
+      :hover{
+        border-bottom: 3px solid rgba(80, 80, 80);
+        line-height: 25.5px;
+        color: black;
+      }
+    }
+  }
+  >p{
+      font-size: 20px;
+      font-weight: 500;
+      color: black;
+  }
+`;
+
+const Button = styled.button`
+padding: 10px 40px;
+margin-bottom: 28px;
+font-size: 20px;
+font-weight: 600;
+letter-spacing: 1.2px;
+color: white;
+background-color: rgba(40, 65, 255, 0.8);
+border-radius: 4px;
+:hover {
+  background-color: rgba(20, 35, 205, 0.8);
+}
+:disabled {
+  background-color: rgba(0, 65, 205, 0.4);
+  cursor: not-allowed;
+}
 `;
