44ebcff986b6b82db09d771e5cefe7e2055ecb7e
Jesse Greenough <jesse.greenough84@gmail.com>
2023-02-07 10:32:38 -0800

    Implement a redirect for a user not signed in

--- src/pages/AccountPage.tsx
+++ src/pages/AccountPage.tsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { useUserData } from '../app/Store/User/userSlice'
 
 const AccountPage = () => {
+  const user = useUserData();
+  const nav = useNavigate()
+  useEffect(() => {
+    if (!user) {
+      nav('/login');
+    }
+  }, [])
+
   return (
     <div>Here Is your account for now!</div>
   )
