efeca7bc95338ec7571ef7d24c5266585c15ca80
Jesse Greenough <jesse.greenough84@gmail.com>
2023-02-08 13:00:47 -0800

    Move display styled components in with the regular display components

--- src/components/HomePage/DisplayComponents.ts
+++ src/components/HomePage/DisplayComponents.ts
@@ -19,9 +19,10 @@ export const Container = styled.div`
 export const ButtonWrap = styled.div`
   display: flex;
   position: relative;
-  top: 240px;
+  top: 16vh;
   @media (max-width: 744px) {
     flex-direction: column;
+    top: 8vh;
   }
 `;
 
@@ -30,25 +31,30 @@ export const Button = styled.button`
   justify-content: center;
   align-items: center;
   position: relative;
-  top: 50px;
-  margin: 8px 16px;
-  width: 17vw;
-  min-width: 35vw;
-  @media (max-width: 744px) {
+  top: 6vh;
+  margin: 2vh 16px;
+  width: 36vw;
+  max-width: 500px;
+  @media only screen and (max-width: 744px) {
     flex-direction: column;
-    min-width: 90vw;
-    margin: 12px 0px;
+    min-width: 80vw;
+    margin: 1.5vh 0px;
+    height: 50px;
   }
-  height: 50px;
+  height: 60px;
   font-size: 20px;
   border-radius: 5px;
   cursor: pointer;
 `;
 
 export const DownArrow = styled.img`
-  margin-bottom: 35px;
-  height: 40px;
-  width: 80px;
+  margin-bottom: 2vh;
+  height: 60px;
+  width: 100px;
   animation: animateDown infinite 1.5s;
   overflow-x: hidden;
+@media only screen and (max-width: 744px){
+    height: 40px;
+    width: 80px;
+}
 `;
