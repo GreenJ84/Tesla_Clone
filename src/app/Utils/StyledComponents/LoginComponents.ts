73c8f418a5512bb888703e9fa73e4b5f2a8cb4cf
Jesse Greenough <jesse.greenough84@gmail.com>
2023-02-08 21:46:03 -0800

    Split common properties from button coponents into a base

--- src/app/Utils/StyledComponents/LoginComponents.ts
+++ src/app/Utils/StyledComponents/LoginComponents.ts
@@ -56,15 +56,22 @@ box-shadow: 0px 10px 20px rgba(20, 20, 20, 0.2);
 z-index: 10;
 `;
 
-export const Button = styled.button`
+export const ButtonBase = styled.button`
 padding: 10px 0;
-margin-bottom: 28px;
 font-size: 20px;
 font-weight: 600;
 letter-spacing: 1.2px;
+border-radius: 4px;
+@media only screen and (max-width: 550px){
+  width: 80vw;
+  margin: 0 auto;
+}
+`;
+
+export const Button = styled(ButtonBase)`
+margin-bottom: 28px;
 color: white;
 background-color: rgba(40, 65, 255, 0.8);
-border-radius: 4px;
 :hover {
   background-color: rgba(20, 35, 205, 0.8);
 }
@@ -72,8 +79,12 @@ border-radius: 4px;
   background-color: rgba(0, 65, 205, 0.4);
   cursor: not-allowed;
 }
-@media only screen and (max-width: 550px){
-  width: 80vw;
-  margin: 0 auto;
-}
+`;
+
+export const Button2 = styled(ButtonBase)`
+  color: black;
+  background-color: rgba(220, 220, 220, 0.3);
+  :hover {
+    background-color: rgba(220, 220, 220, 0.5);
+  }
 `;
