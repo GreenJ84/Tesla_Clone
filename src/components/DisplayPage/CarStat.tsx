907764073686e4d5a8425aae3aaf98b0b6f795b5
Jesse Greenough <jesse.greenough84@gmail.com>
2023-02-08 22:55:25 -0800

    Update component names
    Migrate styled components to assigned files
    
    Update imports

--- src/components/DisplayPage/CarStat.tsx
+++ src/components/DisplayPage/CarStat.tsx
@@ -1,6 +1,6 @@
 /** @format */
 
-import styled from "styled-components";
+import { CarStatContainer } from "../../app/Utils/StyledComponents/DetailComponents";
 
 interface CarStats {
     value: string
@@ -10,33 +10,15 @@ interface CarStats {
 const CarStat = (props: CarStats) => {
     const { value, description } = props;
     return (
-    <Container>
+    <CarStatContainer>
             <h1>
                 {value}
             </h1>
             <h2>
                 {description}
             </h2>
-    </Container>
+    </CarStatContainer>
     );
 };
 
 export default CarStat;
-
-const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    h1{
-        color: white;
-        font-size: 36px;
-        font-weight: 600;
-        text-shadow: 0px 0px 8px rgba(10,10,10);
-    }
-    h2{
-        margin-top: 6px;
-        color: lightgrey;
-        font-size: 18px;
-    }
-`;
