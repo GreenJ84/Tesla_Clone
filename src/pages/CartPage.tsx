447f62e253d512a13f91c90a74515dafe07d8186
Jesse Greenough <jesse.greenough84@gmail.com>
2023-02-09 01:12:38 -0800

    Update imports
    Prepare styled coponents for migration

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,14 +1,14 @@
 /** @format */
 
 import { useDispatch } from "react-redux";
-import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
+import Header from "../components/Layout/Header";
 import OrderSummary from "../components/CartPage/OrderSummary";
+import CartItem from "../components/CartPage/CartItem";
+import { Cart, CartContainer, Checkout, EmptyCart, OrderBody } from "../app/Utils/StyledComponents/OrderComponents";
 
 import { useCartState } from "../app/Utils/hooks/useCartState";
-import CartItem from "../components/CartPage/CartItem";
-import Header from "../components/Layout/Header";
-import { useNavigate } from "react-router-dom";
 import { setTotal } from "../app/Store/Car/carSlice";
 
 const CartPage = () => {
@@ -71,97 +71,3 @@ const CartPage = () => {
 };
 
 export default CartPage;
-export const OrderBody = styled.div`
-@media screen and (min-width: 1280px) {
-  padding: 0 40px;
-}
-@media screen and (max-width: 1280px) {
-  width: 700px;
-  padding-bottom: 200px;
-}
-@media screen and (max-width: 750px) {
-  width: 90vw;
-}
-`;
-
-export const CartContainer = styled.div`
-  display: flex;
-  margin-bottom: 200px;
-  max-width: 1400px;
-  margin: 0 auto;
-  @media screen and (min-width: 1280px) {
-    justify-content: space-between;
-  }
-  @media screen and (max-width: 1280px) {
-    flex-direction: column;
-  }
-`;
-
-export const Cart = styled.div`
-  border-bottom: 1.5px solid rgba(225, 225, 225);
-  @media screen and (min-width: 1280px) {
-    width: 640px;
-    margin-right: 35px;
-    border: none;
-  }
-`;
-
-export const EmptyCart = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  h3{
-    font-size: 28px;
-    font-weight: 400;
-    letter-spacing: 1.2px;
-    margin: 20px 0 40px;
-  }
-  button{
-    padding: 10px 0;
-    margin-bottom: 40px;
-    width: 30%;
-    font-size: 18px;
-    font-weight: 600;
-    letter-spacing: 1.2px;
-    color: white;
-    background-color: rgba(20, 65, 255, 0.8);
-    border-radius: 4px;
-  }
-  button:hover {
-    background-color: rgba(0, 35, 205, 0.8);
-  }
-`;
-
-export const Checkout = styled.div`
-  visibility: hidden;
-  position: fixed;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100vw;
-  left:0;
-  padding: 20px 0;
-  background-color: white;
-  box-shadow: 0 0 10px rgba(200, 200, 200);
-  button {
-    padding: 10px 0;
-    width: 640px;
-    max-width: 90vw;
-    font-size: 20px;
-    font-weight: 600;
-    letter-spacing: 1.2px;
-    color: white;
-    background-color: rgba(20, 65, 255, 0.8);
-    border-radius: 4px;
-  }
-  button:hover {
-    background-color: rgba(0, 35, 205, 0.8);
-  }
-  button:disabled {
-    background-color: rgba(0, 35, 205, 0.4);
-  }
-  bottom: 0;
-  @media screen and (max-width: 1280px) {
-    visibility: visible;
-  }
-`;
