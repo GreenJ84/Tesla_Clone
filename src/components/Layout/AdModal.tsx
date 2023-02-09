f79bca0c22c25e1e6bcb046101b6bc5ca989bd1f
Jesse Greenough <jesse.greenough84@gmail.com>
2023-02-08 22:30:37 -0800

    Migrate styled components to assigned file Update imports

--- src/components/Layout/AdModal.tsx
+++ src/components/Layout/AdModal.tsx
@@ -2,15 +2,13 @@
 
 import React, { useState } from "react";
 import { XMarkIcon } from "@heroicons/react/24/solid";
-import styled from "styled-components";
-import { Close } from "../../app/Utils/StyledComponents/HeaderComponents";
-import { StyledProps } from "../../app/Utils/StyledComponents/HeaderComponents";
+import { AdModalContainer, Close } from "../../app/Utils/StyledComponents/LayoutComponents";
 
 const AdModal = () => {
     const [modalStatus, setModalStatus] = useState(true)
 
     return (
-    <Container show={modalStatus}>
+    <AdModalContainer show={modalStatus}>
         <Close>
           <XMarkIcon
             style={{ cursor: "pointer" }}
@@ -21,31 +19,8 @@ const AdModal = () => {
         <p>
             Until March 2023, certain new Model 3 and Model Y vehicles qualify for a $7,500 federal tax credit for eligible buyers. <span>Learn More</span>
         </p>
-    </Container>
+    </AdModalContainer>
     );
 };
 
 export default AdModal;
-
-const Container = styled.div`
-    position: sticky;
-    display: ${({ show }: StyledProps) => 
-    show ? "block" : "none"
-    };
-    top: 0;
-    width: 100vw;
-    height: 100px;
-    font-size: 22px;
-    text-align: center;
-    p{
-        position: relative;
-        top: -8px;
-        span{
-            cursor: pointer;
-            border-bottom: 1.5px solid black;
-        }
-        span:hover{
-            border-bottom: 3px solid black;
-        }
-    }
-`;
