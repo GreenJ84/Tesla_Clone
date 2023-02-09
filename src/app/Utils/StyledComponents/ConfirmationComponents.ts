1b9d1c58975532b78adad1a5ae4201755f4987b5
Jesse Greenough <jesse.greenough84@gmail.com>
2023-02-08 23:18:06 -0800

    Update imports
    Format file

--- src/app/Utils/StyledComponents/ConfirmationComponents.ts
+++ src/app/Utils/StyledComponents/ConfirmationComponents.ts
@@ -1,43 +1,47 @@
+/** @format */
+
+import styled from "styled-components";
+
 export const ConfirmationContainer = styled.div`
   position: relative;
   width: 80vw;
   max-width: 800px;
   margin: 0 auto;
   padding-top: 100px;
-  >h1{
+  > h1 {
     font-size: 40px;
     font-weight: 600;
     margin-bottom: 16px;
   }
-  >p{
+  > p {
     font-size: 20px;
     font-weight: 500;
     margin-bottom: 60px;
   }
-  span{
+  span {
     cursor: pointer;
     border-bottom: 1.2px solid black;
-    :hover{
+    :hover {
       border-bottom: 2px solid black;
     }
   }
-  >div:first-of-type{
-    >p{
+  > div:first-of-type {
+    > p {
       font-size: 24px;
       margin-bottom: 20px;
-      >li{
+      > li {
         position: relative;
         left: 40px;
         margin-top: 10px;
       }
     }
-    >p:nth-of-type(3){
+    > p:nth-of-type(3) {
       font-size: 24px;
       font-weight: 600;
       letter-spacing: 1.1px;
     }
   }
-  >div:nth-of-type(2){
+  > div:nth-of-type(2) {
     border-top: 1px solid rgba(200, 200, 200);
     border-bottom: 1px solid rgba(200, 200, 200);
   }
@@ -49,33 +53,33 @@ export const ConfirmationListItem = styled.li`
   margin: 40px 0;
   min-height: 200px;
   align-items: center;
-  >img{
+  > img {
     width: 160px;
     height: 160px;
     margin-right: 20px;
   }
-  >div{
-    >div{
+  > div {
+    > div {
       display: flex;
       justify-content: space-between;
-      >h3{
+      > h3 {
         font-size: 26px;
         font-weight: 500;
         margin-bottom: 5px;
-        :nth-of-type(2){
+        :nth-of-type(2) {
           font-weight: 700;
         }
       }
     }
-    >p{
+    > p {
       font-size: 18px;
       line-height: 20px;
       margin-bottom: 10px;
     }
   }
-@media only screen and (max-width: 800px){
-  >img{
-    align-self: start;
+  @media only screen and (max-width: 800px) {
+    > img {
+      align-self: start;
+    }
   }
-}
 `;
