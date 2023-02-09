a358fb16037c052a57b9138c265118e7dfd18ee1
Jesse Greenough <jesse.greenough84@gmail.com>
2023-02-08 22:33:23 -0800

    Update component namain Update imports

--- src/components/Layout/MinimalHeader.tsx
+++ src/components/Layout/MinimalHeader.tsx
@@ -6,30 +6,30 @@ import { GlobeAltIcon } from "@heroicons/react/24/outline";
 
 
 import {
-  Container,
+  NavContainer,
   SideMenu,
-} from "../../app/Utils/StyledComponents/HeaderComponents";
+} from "../../app/Utils/StyledComponents/LayoutComponents";
 
 export const LogRegHeader = () => {
 
   return (
-    <Container>
+    <NavContainer>
       {/* Tesla Logo Display */}
       <Link to="/">
         <img
-          className="relative top-2 left-2 w-40 h-5 xl:w-48 2xl:top-5 2xl:left-5 2xl:w-52"
+          className="relative top-2 left-2 w-24 md:w-40 md:h-5 xl:w-48 2xl:top-5 2xl:left-5 2xl:w-52"
           src="/images/logo.svg"
           alt="Tesla Logo"
         />
       </Link>
       <SideMenu>
         <p
-          className="cursor bg-transparent hover:bg-[rgba(220,220,220,.5)] transition-colors duration-700 pl-1 pr-3 py-4 mt-2 text-lg text-black font-semibold rounded-[6px]"
+          className="cursor bg-transparent hover:bg-[rgba(220,220,220,.5)] transition-colors duration-700 pl-1 pr-1 py-4 text-lg text-black font-semibold rounded-[6px]"
         >
           <GlobeAltIcon className="inline w-9 h-7"/>en-US
         </p>
       </SideMenu>
-    </Container>
+    </NavContainer>
   );
 };
 
