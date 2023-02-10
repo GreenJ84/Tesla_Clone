8aaae68ac48a67d36c3d4cd81a343e791af3c12a
Jesse Greenough <jesse.greenough84@gmail.com>
2023-02-09 22:39:58 -0800

    Update styled component stylings and naming

--- src/components/Layout/SmallFooter.tsx
+++ src/components/Layout/SmallFooter.tsx
@@ -3,34 +3,38 @@ import styled from 'styled-components';
 
 const SmallFooter = () => {
   return (
-    <Container >
+    <SmallFooterContainer >
         <p>Tesla © 2023</p>
         <p>Privacy & Legal</p>
         <p>Contact</p>
-    </Container>
+    </SmallFooterContainer>
   )
 }
 
 export default SmallFooter
 
-const Container = styled.div`
-    position: absolute;
-    bottom: 0;
-    left: 50%;
-    transform: translateX(-50%);
-    display: flex;
-    justify-content:space-around;
-    margin-bottom: 10px;
-    width: 360px;
-    p{
-        font-weight: 550;
-        color: rgba(110,110,110);
-        margin-bottom: 24px;
-        line-height: 18px;
-    }
-    p:hover{
-        color: black;
-        border-bottom: 3px solid black;
-        margin-bottom: 21px;
-    }
+const SmallFooterContainer = styled.footer`
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  display: flex;
+  justify-content:space-around;
+  width: 100%;
+  max-width: 500px;
+  p{
+    font-size: 22px;
+    font-weight: 550;
+    color: rgba(110,110,110);
+    margin-bottom: 20px;
+    line-height: 18px;
+  }
+  p:hover{
+    color: black;
+    border-bottom: 2px solid black;
+    line-height: 16px;
+  }
+@media only screen and (min-width: 1400px){
+  max-width: 600px;
+}
 `;
