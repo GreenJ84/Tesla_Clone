c0e6af5c2719958d4c5e931deef274eb972ef889
Jesse Greenough <jesse.greenough84@gmail.com>
2023-02-10 04:51:41 -0800

    Create a full page navigation popup

---
+++ src/components/AccountPage/PopupMenu.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import styled from 'styled-components';
+import { XMarkIcon } from '@heroicons/react/24/solid';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router';
+import NavList, { IconStyle } from './NavList';
+
+interface AccountPopup {
+    toggle: Function
+}
+
+const PopupMenu = (props: AccountPopup) => {
+    const { toggle } = props;
+
+  return (
+      <PopupMenuContainer>
+        <XMarkIcon 
+            onClick={toggle()}
+            className={ CloseIcon } 
+        />
+        <NavList wide={true}/>
+      </PopupMenuContainer>
+  )
+}
+
+export default PopupMenu;
+
+const PopupMenuContainer = styled.div`
+    position: fixed;
+    z-index: 2000;
+    width: 100vw;
+    height: 100vh;
+    padding: 86px 0 0 30px;
+    background-color: rgba(240, 240, 240, .94);
+    >ul{
+        list-style: none;
+        >li{
+            cursor: pointer;
+            display: flex;
+            align-items: center;
+            margin-bottom: 40px;
+            font-size: 25px;
+            font-weight: 600;
+            letter-spacing: 1.15px;
+            color: rgba(90,90,90);
+            :hover{
+                color: black;
+            }
+        }
+    }
+`;
+
+const CloseIcon = `absolute ${IconStyle} lg:w-12 lg:h-12 top-4 right-4`;
