0d0244768eba98f316c5a188ad8cadd634f012ff
Jesse Greenough <jesse.greenough84@gmail.com>
2023-05-18 23:55:13 -0700

    Remove firbase setup and export for modularization

--- src/index.tsx
+++ src/index.tsx
@@ -7,43 +7,23 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
 import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { persistor, store } from "./app/Store/store";
 
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
-
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth, GoogleAuthProvider, FacebookAuthProvider, TwitterAuthProvider, GithubAuthProvider, OAuthProvider } from "firebase/auth";
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCYjBCaFh6cbvKYvu6s4sGJl3KLk4NVArw",
-  authDomain: "tesla-gclone.firebaseapp.com",
-  projectId: "tesla-gclone",
-  storageBucket: "tesla-gclone.appspot.com",
-  messagingSenderId: "248739858073",
-  appId: "1:248739858073:web:12b0ab828326283f4cda08",
-};
-export const app = initializeApp(firebaseConfig);
-
-export const googleProvider = new GoogleAuthProvider();
-export const facebookProvider = new TwitterAuthProvider();
-export const twitterProvider = new FacebookAuthProvider();
-export const githubProvider = new GithubAuthProvider();
-export const appleProvider = new OAuthProvider('apple.com');
-
-export const AUTH = getAuth(app);
-export const DB = getFirestore(app);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
+        <PersistGate
+          loading={null}
+          persistor={persistor}
+        >
           <App />
         </PersistGate>
       </Provider>
