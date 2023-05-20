bd459c21c9af94532f8711e3acb6968c0c038c0c
Jesse Greenough <jesse.greenough84@gmail.com>
2023-05-20 15:05:48 -0700

    Update page referencing login & element semantics

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,14 @@
 /** @format */
 
 import React, { useRef } from "react";
-import Header from "../components/Layout/Header";
-import AdModal from "../components/Layout/AdModal";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
 
+import Header from "../components/Layout/Header";
+import AdModal from "../components/Layout/AdModal";
 import Display from "../components/HomePage/Display";
 
 import { selectCars } from "../app/Store/Car/carSlice";
-
 
 const HomePage = () => {
   const homeRef = useRef<HTMLDivElement>(null);
@@ -19,15 +18,14 @@ const HomePage = () => {
     <>
       <AdModal />
       <div style={{ position: "relative" }}>
-        <Header />
+        <Header homeRef={homeRef} />
         <Container ref={homeRef}>
           {cars.map((car) => (
-              <Display
-                id={car.id}
-                title={car.title}
-                homeRef={homeRef}
-                backgroundImg={car.backgroundImg}
-              />
+            <Display
+              id={car.id}
+              title={car.title}
+              backgroundImg={car.backgroundImg}
+            />
           ))}
         </Container>
       </div>
@@ -37,7 +35,7 @@ const HomePage = () => {
 
 export default HomePage;
 
-const Container = styled.div`
+const Container = styled.main`
   overflow-y: auto;
   text-align: center;
   height: 100vh;
