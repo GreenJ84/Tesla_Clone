630f3515d8278bb28d6f5fafb2729bbd984daef4
Jesse Greenough <jesse.greenough84@gmail.com>
2023-05-20 17:06:53 -0700

    Format and Refactor components - Update Semantics of elements used - Update ARIA for elements used - Update scrolling implementations

--- src/components/HomePage/Display.tsx
+++ src/components/HomePage/Display.tsx
@@ -1,47 +1,34 @@
 /** @format */
 
-import React, { useEffect, useRef } from "react";
-import { Link } from "react-router-dom";
+import React, { BaseSyntheticEvent } from "react";
+import { useNavigate } from "react-router-dom";
 import { Fade } from "react-awesome-reveal";
 
-import { selectCars, setCarRef } from "../../app/Store/Car/carSlice";
-import { useAppDispatch } from "../../app/Utils/hooks/useAppDispatch";
+import { selectCars } from "../../app/Store/Car/carSlice";
 import { useAppSelector } from "../../app/Utils/hooks/useAppSelector";
 
-import {
-  Container,
-  ButtonWrap,
-  Button,
-  DownArrow,
-} from "./DisplayComponents";
+import { Container, ButtonWrap, Button, DownArrow } from "./DisplayComponents";
 
 interface DisplayProps {
   id: number;
   title: string;
   backgroundImg: string;
-  homeRef: React.RefObject<HTMLDivElement>;
+  slide: (e: BaseSyntheticEvent, idx: number) => void;
 }
 
 const Display = (props: DisplayProps) => {
-  let myRef = useRef<HTMLDivElement>(null);
-  const dispatch = useAppDispatch();
+  const nav = useNavigate();
   const cars = useAppSelector(selectCars);
-
-  useEffect(() => {
-    if (myRef.current) {
-      dispatch(setCarRef({ id: props.id, ref: myRef }));
-    }
-  }, [myRef.current]);
 
   return (
     <Container
-      ref={myRef}
       bgImage={props.backgroundImg}
     >
       <Fade direction="up">
         <div style={{ paddingTop: "15vh", textAlign: "center" }}>
           <h1 className="text-6xl font-medium text-black">{props.title}</h1>
           <a
+            aria-label="Offsite link to Tesla's actual test drive registration"
             href="https://www.tesla.com/drive"
             rel="noreferrer"
             target="_blank"
@@ -53,37 +40,38 @@ const Display = (props: DisplayProps) => {
       </Fade>
       <Fade direction="up">
         <ButtonWrap>
-          <Link to={`/cars/${props.id}`}>
-            <Button className="text-white bg-[rgba(20,20,20,.85)] opacity-95 hover:animate-bounce">
-              Custom Order
-            </Button>
-          </Link>
-          <Link to={`/cart`}>
-            <Button className="text-black bg-[rgba(255,255,255,.7)] opacity-90 hover:animate-bounce">
-              View Inventory
-            </Button>
-          </Link>
+          <Button
+            role="link"
+            aria-label="Car Navigation"
+            className="text-white bg-[rgba(20,20,20,.85)] opacity-95 hover:animate-bounce"
+            onClick={() => {
+              nav("/cars/" + props.id);
+            }}
+          >
+            Custom Order
+          </Button>
+          <Button
+            role="link"
+            aria-label="Cart Navigation"
+            className="text-black bg-[rgba(255,255,255,.7)] opacity-90 hover:animate-bounce"
+            onClick={() => {
+              nav("/cart");
+            }}
+          >
+            View Inventory
+          </Button>
         </ButtonWrap>
       </Fade>
       {props.id !== cars.length ? (
         <DownArrow
-          onClick={() =>
-            props.homeRef?.current &&
-              props.homeRef.current.scrollTo({
-                behavior: "smooth",
-                top: cars[props.id].ref!.current ?
-                    cars[props.id].ref!.current!.offsetTop
-                  :
-                    window.innerHeight * props.id
-              })
-          }
+          role="link"
+          aria-label="Next Car Window Slide"
+          onClick={(e: BaseSyntheticEvent) => props.slide(e, props.id)}
           className="cursor mx-auto"
           src="images/down-arrow.svg"
         />
       ) : (
-        <div
-          style={{ marginBottom: "35px", height: "40px", width: "80px" }}
-        ></div>
+        ""
       )}
     </Container>
   );
