bea696ec14aa9a1b08fe1593ed01b502404372ce
Jesse Greenough <jesse.greenough84@gmail.com>
2023-05-22 04:01:03 -0700

    Update imports, add aria attributes, and move styled components to Acount components file
    Format, Clean, and Organize code

--- src/components/AccountPage/PopupMenu.tsx
+++ src/components/AccountPage/PopupMenu.tsx
@@ -1,46 +1,30 @@
-import React from 'react'
-import styled from 'styled-components';
-import { XMarkIcon } from '@heroicons/react/24/solid';
-import NavList, { IconStyle } from './NavList';
+/** @format */
 
-const PopupMenu = (props: {toggle: Function}) => {
+import React from "react";
 
+import { XMarkIcon } from "@heroicons/react/24/solid";
+
+import NavList from "./NavList";
+import {
+  CloseIcon,
+  PopupMenuContainer,
+} from "../../app/Utils/StyledComponents/AccountComponents";
+
+const PopupMenu = (props: { toggle: Function }) => {
   return (
-      <PopupMenuContainer>
-        <XMarkIcon 
-            onClick={props.toggle()}
-            className={ CloseIcon } 
-        />
-          <NavList toggle={props.toggle()} wide={true}/>
-      </PopupMenuContainer>
-  )
-}
+    <PopupMenuContainer>
+      <XMarkIcon
+        role="button"
+        aria-label="Close Menu"
+        onClick={props.toggle()}
+        className={CloseIcon}
+      />
+      <NavList
+        toggle={props.toggle()}
+        wide={true}
+      />
+    </PopupMenuContainer>
+  );
+};
 
 export default PopupMenu;
-
-const PopupMenuContainer = styled.div`
-    position: fixed;
-    z-index: 2000;
-    width: 100vw;
-    height: 100vh;
-    padding: 86px 0 0 30px;
-    background-color: rgba(240, 240, 240, .94);
-    >ul{
-        list-style: none;
-        >li{
-            cursor: pointer;
-            display: flex;
-            align-items: center;
-            margin-bottom: 40px;
-            font-size: 25px;
-            font-weight: 600;
-            letter-spacing: 1.15px;
-            color: rgba(90,90,90);
-            :hover{
-                color: black;
-            }
-        }
-    }
-`;
-
-const CloseIcon = `absolute ${IconStyle} lg:w-12 lg:h-12 top-4 right-4`;
