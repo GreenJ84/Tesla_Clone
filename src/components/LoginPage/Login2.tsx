f59634dfd5439bcab2630961957f17745d8b5f54
Jesse Greenough <jesse.greenough84@gmail.com>
2023-05-21 23:48:06 -0700

    Add password tooltip, add password toggling, update component aria attributes, update component layout
    Format, clean, and organize code

--- src/components/LoginPage/Login2.tsx
+++ src/components/LoginPage/Login2.tsx
@@ -1,7 +1,16 @@
 /** @format */
 
-import React from "react";
-import { Button, LoginContainer, Underline } from "../../app/Utils/StyledComponents/LoginComponents";
+import React, { useEffect, useState } from "react";
+
+import { InformationCircleIcon } from "@heroicons/react/24/outline";
+
+import {
+  Button,
+  LoginContainer,
+  Underline,
+} from "../../app/Utils/StyledComponents/LoginComponents";
+import { RegToolTip } from "../../app/Utils/StyledComponents/RegisrationComponents";
+import Show from "../Layout/Show";
 
 interface Login2Props {
   password: [string, Function];
@@ -18,7 +27,29 @@ const Login2 = (props: Login2Props) => {
     login(e);
   };
 
+  const [showPass, setShowPass] = useState(false);
+  const showPassHandler = () => {
+    showPass ? setShowPass(false) : setShowPass(true);
+  };
 
+  useEffect(() => {
+    let passHover = document.getElementById("pass-hover")!;
+    let passTip = document.getElementById("pass-tip")!;
+
+    const displayTip = () => {
+      passTip.style.visibility = "visible";
+    };
+    const hideTip = () => {
+      passTip.style.visibility = "hidden";
+    };
+
+    passHover.addEventListener("mouseover", () => displayTip());
+    passHover.addEventListener("mouseout", () => hideTip());
+    return () => {
+      passHover.removeEventListener("mouseover", () => displayTip());
+      passHover.removeEventListener("mouseout", () => hideTip());
+    };
+  }, []);
 
   return (
     <LoginContainer>
@@ -26,33 +57,86 @@ const Login2 = (props: Login2Props) => {
         htmlFor="password"
         className="relative"
       >
-        Password
+        Password<span aria-hidden="true">*</span>
+        <InformationCircleIcon
+          id="pass-hover"
+          className="relative inline bottom-[3px] w-4 h-4 ml-1"
+        />
+        <RegToolTip
+          id="pass-tip"
+          className="invisible"
+        >
+          Password must be at least 8 characters and include at least one number
+          and one letter
+        </RegToolTip>
       </label>
       <input
-        type="password"
+        id="password"
         name="password"
+        aria-label="A Password 8 or more character long"
+        aria-describedby="pass-error"
+        className={`relative ${
+          password ? "w-[430px] border invalid:border-red-500" : "w-[430px]"
+        }`}
+        type={showPass ? "text" : "password"}
         value={password}
+        placeholder="Ex. not!aBadpasword99x84!"
         onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
           setPassword(e.currentTarget.value)
         }
-        className="w-[430px]"
+        pattern="(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])\S{8,}"
+        aria-invalid={
+          password.match(
+            /(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])\S{8,}/g
+          )
+            ? "false"
+            : "true"
+        }
         required
-        pattern="([A-Za-z!#$%&]+[1-9]+){8,}"
+        aria-required="true"
       />
-      {error ? <div>{error}</div> : ""}
+      {password && (
+        <Show
+          state={showPass}
+          set={showPassHandler}
+        />
+      )}
+      {error && (
+        <div
+          id="pass-error"
+          role="alert"
+          aria-live="assertive"
+          aria-atomic="true"
+        >
+          {error}
+        </div>
+      )}
+
       {password ? (
-        <Button onClick={buttonHandler}>Sign In</Button>
+        <Button
+          type="submit"
+          aria-label="Sign In"
+          onClick={buttonHandler}
+        >
+          Sign In
+        </Button>
       ) : (
-        <Button disabled>Sign In</Button>
+        <Button
+          disabled
+          aria-disabled="true"
+          aria-label="Account requirements not met"
+        >
+          Sign In
+        </Button>
       )}
       <div className="relative mb-[76px]">
-      <Underline
-        href="/"
-        rel="noreferrer"
-        target="_blank"
-      >
-        Forgot Password?
-      </Underline>
+        <Underline
+          href="/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          Forgot Password?
+        </Underline>
       </div>
     </LoginContainer>
   );
