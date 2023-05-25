d6ccb1bdd8624becf2cab731a915d864df9f3b97
Jesse Greenough <jesse.greenough84@gmail.com>
2023-05-24 22:21:35 -0700

    Update webpack to reduce excessive bundling and compression during development

--- webpack.config.ts
+++ webpack.config.ts
@@ -9,10 +9,11 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
 const TerserPlugin = require('terser-webpack-plugin');
 const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 
+const isProduction = process.env.NODE_ENV === "production";
 
 
-module.exports = {
-    mode: "production", // Set the mode to 'production' for optimized builds
+const config = {
+    mode: "development",
     entry: {
         main: "./src/index.tsx",
         home: "./src/pages/HomePage.tsx",
@@ -28,7 +29,7 @@ module.exports = {
     filename: 'bundle.[contenthash].js', // Output filename
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"], // Resolve these extensions
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
   },
   devtool: "source-map",
   devServer: {
@@ -45,37 +46,14 @@ module.exports = {
       {
         test: /\.(js|jsx|tsx|ts)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"], // Use Babel to transpile TypeScript/JSX files
-    },
-    {
+        use: ["babel-loader"],
+      },
+      {
         test: /\.css$/,
+        exclude: /node_modules/,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        type: "asset/resource",
-        use: [
-          'file-loader', // or 'url-loader'
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              mozjpeg: {
-                quality: 80,
-                progressive: true,
-              },
-              pngquant: {
-                quality: '65-90',
-                speed: 4,
-              }
-            },
-          },
-        ],
-      },
-      {
-        test: /\.json$/,
-        use: 'file-loader',
-      },
-    ],
+      }
+    ] as any[],
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -93,27 +71,19 @@ module.exports = {
         },
       ],
     }),
-    new MiniCssExtractPlugin(),
-    new CompressionPlugin({
-      algorithm: 'gzip', // Use gzip compression
-      filename: '[path][base].gz', // Output filename pattern
-      test: /\.(js|css|html)$/, // Apply compression to JavaScript, CSS, and HTML files
-      threshold: 10240, // Only compress files larger than 10KB
-      minRatio: 0.8, // Only compress files with a compression ratio of at least 0.8 (80%)
-    }),
-    // new BundleAnalyzerPlugin(),
+    new MiniCssExtractPlugin()
   ],
   optimization: {
     usedExports: true,
-    minimize: true, // Enable minification
+    minimize: true,
     minimizer: [
       new TerserPlugin({
         terserOptions: {
           format: {
-            comments: false, // Remove comments from the output
+            comments: false,
           },
           compress: {
-            dead_code: true, // Remove dead code from the output
+            dead_code: true,
             drop_console: true,
             drop_debugger: true,
             warnings: true
@@ -121,22 +91,69 @@ module.exports = {
         },
         extractComments: false,
       }),
-      new ImageMinimizerPlugin({
-        minimizer: {
-          implementation: ImageMinimizerPlugin.imageminMinify,
-          options: {
-            plugins: [
-              ["jpegtran", { progressive: true }],
-              ["optipng", { optimizationLevel: 5 }],
-            ]
-          },
-        },
-      }),
     ],
     splitChunks: {
-        chunks: "all", // Enable code splitting for all chunks
+        chunks: "all",
         minSize: 244 * 1024,
     },
     runtimeChunk: 'single',
   },
 };
+
+if (isProduction) {
+  // JSON loading
+  config.module.rules.push({
+    test: /\.json$/,
+    use: 'file-loader',
+  })
+
+  // Image minification
+  config.module.rules.push({
+    test: /\.(jpe?g|png|gif|svg)$/i,
+    type: "asset/resource",
+    use: [
+      'file-loader', // or 'url-loader'
+      {
+        loader: 'image-webpack-loader',
+        options: {
+          mozjpeg: {
+            quality: 80,
+            progressive: true,
+          },
+          pngquant: {
+            quality: '65-90',
+            speed: 4,
+          }
+        },
+      },
+    ],
+  })
+  config.optimization.minimizer.push(new ImageMinimizerPlugin({
+    minimizer: {
+      implementation: ImageMinimizerPlugin.imageminMinify,
+      options: {
+        plugins: [
+          ["jpegtran", { progressive: true }],
+          ["optipng", { optimizationLevel: 5 }],
+        ]
+      },
+    },
+  }))
+  
+
+  // Compression
+  config.plugins.push(new CompressionPlugin({
+    algorithm: 'gzip', // Use gzip compression
+    filename: '[path][base].gz', // Output filename pattern
+    test: /\.(js|css|html)$/, // Apply compression to JavaScript, CSS, and HTML files
+    threshold: 10240, // Only compress files larger than 10KB
+    minRatio: 0.8, // Only compress files with a compression ratio of at least 0.8 (80%)
+  }))
+
+  // Debugging
+  config.plugins.push(new BundleAnalyzerPlugin())
+}
+
+
+
+module.exports = config;
