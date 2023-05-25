ffdcb7c05198277d9e11b67b1f2889ff71a90bf4
Jesse Greenough <jesse.greenough84@gmail.com>
2023-05-25 00:49:25 -0700

    Add Adress validation functions

--- src/app/Store/Order/orderSlice.ts
+++ src/app/Store/Order/orderSlice.ts
@@ -87,3 +87,72 @@ export const { setShipping, setBilling, setPayment, setOrder } =
   orderSlice.actions;
 
 export default orderSlice.reducer;
+
+const validateAddress = (address: Address, errors: Address): boolean => { 
+  let noErrors = true;
+  // First Name Validation
+  if (!address.firstName.match(/^[A-Za-z]{2,}$/)) { 
+    noErrors = false;
+    errors.firstName = "First Name must be 2 or more characters";
+  } else { errors.firstName = ""; }
+  // Last Name Validation
+  if (!address.lastName.match(/^[A-Za-z]{2,}$/)) { 
+    noErrors = false;
+    errors.lastName = "Last Name must be 2 or more characters";
+  } else { errors.lastName = ""; }
+  // Address 1 Validation
+  if (!address.address1.match(/^[A-Za-z0-9\s-.,]*$/)) { 
+    noErrors = false;
+    errors.address1 = "Street Address must only be alphanumeric characters, hyphens, commas, periods, or spaces";
+  } else { errors.address1 = ""; }
+  // Address 2 Validation
+  if (!address.address2.match(/^[A-Za-z0-9\s-.,#]*$/)) { 
+    noErrors = false;
+    errors.address1 = "Building Address must only be alphanumeric characters, hyphens, commas, periods, pound signs or spaces";
+  } else { errors.address2 = ""; }
+  // City Validation
+  if (!address.city.match(/^[A-Za-z\s-.]*$/)) { 
+    noErrors = false;
+    errors.city = "City must only be alphanumeric characters, spaces, and hyphens";
+  } else { errors.city = ""; }
+  // State Validation
+  if (!address.state.match(/^[A-Za-z]{2}$/)) { 
+    noErrors = false;
+    errors.state = "State must only be alphabetic characters";
+  } else { errors.state = ""; }
+  // Zip Validation
+  if (!address.zip.match(/^\d{5}(-\d{4})?$/)) { 
+    noErrors = false;
+    errors.zip = "Zip code must be 5 digits or 9 digits seperated with a hyphen";
+  } else { errors.zip = ""; }
+  return noErrors;
+}
+
+export const validateShipping = (shipping: Address, errors: Address): boolean => { 
+  let noErrors = true;
+  noErrors = validateAddress(shipping, errors);
+  // Validate Phone Number
+  if (!shipping.phone?.match(/^[A-Za-z0-9\s-._,#]*$/)) {
+    noErrors = false;
+    errors.phone = "Phone number must be in a correct format with optional leading + seperated by hypens, periods, or spaces";
+  } else { errors.phone = ""; }
+
+  return noErrors;
+}
+
+export const validateBilling = (billing: Address, errors: Address): boolean => { 
+  let noErrors = true;
+  noErrors = validateAddress(billing, errors);
+  // Validate Company Name
+  if (!billing.company?.match(/^$/)) {
+    noErrors = false;
+    errors.company = "Company name must must only be alphanumeric characters, hyphens, commas, periods, pound signs or spaces";
+  } else { errors.company = ""; }
+  // Validate Country Name
+  if (!billing.country?.match(/^[A-Za-z]{2}$/)) {
+    noErrors = false;
+    errors.country = "Country must only be alphabetic characters";
+  } else { errors.country = ""; }
+
+  return noErrors;
+}
