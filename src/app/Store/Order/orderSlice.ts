c526af463937e51419d56d563f719e0f6bce2d20
Jesse Greenough <jesse.greenough84@gmail.com>
2023-05-24 22:30:43 -0700

    Update interfaces, types, and Format code

--- src/app/Store/Order/orderSlice.ts
+++ src/app/Store/Order/orderSlice.ts
@@ -13,66 +13,69 @@ export interface Address {
   city: string;
   state: string;
   country?: string;
+  phone?: string;
+  company?: string;
 }
 export interface Payment {
-    cardHolderName: string;
-    cardNumber: string;
-    cvv: string;
-    expMonth: string;
-    expYear: string;
-    postalCode: string;
+  cardHolderName: string;
+  cardNumber: string;
+  cvv: string;
+  expMonth: string;
+  expYear: string;
+  postalCode: string;
 }
 export interface OrderState {
-    shipping: Address;
-    billing: Address;
-    payment: Payment;
-    order: CartState;
-
+  shipping: Address;
+  billing: Address;
+  payment: Payment;
+  order: CartState;
 }
 
-const initialOrderState: OrderState = {
-    shipping: {
-        firstName: "",
-        lastName: "",
-        address1: "",
-        address2: "",
-        zip: "",
-        city: "",
-        state: "",
-    },
-    billing: {
-        firstName: "",
-        lastName: "",
-        address1: "",
-        address2: "",
-        zip: "",
-        city: "",
-        state: "",
-        country: "",
-    },
-    payment: {
-        cardHolderName: "",
-        cardNumber: "",
-        cvv: "",
-        expMonth: "",
-        expYear: "",
-        postalCode: ""
-    },
-    order: initialCartState,
+export const initialOrderState: OrderState = {
+  shipping: {
+    firstName: "",
+    lastName: "",
+    address1: "",
+    address2: "",
+    zip: "",
+    city: "",
+    state: "",
+    phone: "",
+  },
+  billing: {
+    firstName: "",
+    lastName: "",
+    address1: "",
+    address2: "",
+    zip: "",
+    city: "",
+    state: "",
+    country: "",
+    company: "",
+  },
+  payment: {
+    cardHolderName: "",
+    cardNumber: "",
+    cvv: "",
+    expMonth: "",
+    expYear: "",
+    postalCode: "",
+  },
+  order: initialCartState,
 };
 
 export const orderSlice = createSlice({
   name: "order",
   initialState: initialOrderState,
   reducers: {
-    setShipping: (state, action) => { 
-        state.shipping = action.payload;
+    setShipping: (state, action) => {
+      state.shipping = action.payload;
     },
-    setBilling: (state, action) => { 
-        state.billing = action.payload;
+    setBilling: (state, action) => {
+      state.billing = action.payload;
     },
-    setPayment: (state, action) => { 
-        state.payment = action.payload;
+    setPayment: (state, action) => {
+      state.payment = action.payload;
     },
     setOrder: (state, action) => {
       state.order = action.payload;
@@ -80,6 +83,7 @@ export const orderSlice = createSlice({
   },
 });
 
-export const { setShipping, setBilling, setPayment, setOrder } = orderSlice.actions;
+export const { setShipping, setBilling, setPayment, setOrder } =
+  orderSlice.actions;
 
 export default orderSlice.reducer;
