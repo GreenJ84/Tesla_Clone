2d16e8e4c4ca92f5a56af0ed83b7e4480bb98596
Jesse Greenough <jesse.greenough84@gmail.com>
2023-05-26 22:11:02 -0700

    Update cart properties, methods, and naming convention

--- src/app/Store/Cart/cartSlice.ts
+++ src/app/Store/Cart/cartSlice.ts
@@ -1,18 +1,21 @@
 /** @format */
 
 import { createSlice } from "@reduxjs/toolkit";
+
 import { carData, carsData } from "../../../teslaCarInfo";
 
 
 export interface CartState {
-  cart: carData[];
+  items: carData[];
   subTotal: number;
+  tax: number;
   total: number;
 }
 
 export const initialCartState: CartState = {
-  cart: [],
+  items: [],
   subTotal: 0,
+  tax: 0,
   total: 0,
 };
 
@@ -20,48 +23,59 @@ export const carSlice = createSlice({
   name: "car",
   initialState: initialCartState,
   reducers: {
-    setTotal: (state, action) => {
-      state.total = action.payload;
-    },
     setQuantity: (state, action) => {
-      let [car] = state.cart.filter((item) => item.id === action.payload.id);
-      let [...cars] = state.cart.filter(
+      let [car] = state.items.filter((item) => item.id === action.payload.id);
+      let [...cars] = state.items.filter(
         (item) => item.id !== action.payload.id
       );
+      if (action.payload.quantity === 0) { 
+        state.items = [...cars];
+        state.subTotal = state.subTotal - car.price * car.quantity;
+        return;
+      }
+      let diff = car.price * car.quantity - car.price * action.payload.quantity;
       car.quantity = action.payload.quantity;
-      state.cart = [...cars, car];
+      state.items = [...cars, car];
+      state.subTotal = state.subTotal + diff;
+      return;
     },
     addToCart: (state, action) => {
-      let car = state.cart.find((item) => {
+      let car = state.items.find((item) => {
         return item.id === action.payload;
       });
       if (car) {
-        let car = state.cart.filter((item) => item.id === action.payload)[0];
-        let [...cars] = state.cart.filter(
+        let [...cars] = state.items.filter(
           (item) => item.id !== action.payload);
         car.quantity += 1;
-        state.cart = [...cars, car];
-      }
-      else {
+        state.items = [...cars, car];
+        state.subTotal = state.subTotal + car.price;
+      } else {
         let [_car] = carsData.filter((item) => item.id === action.payload);
-        state.cart = [...state.cart, _car];
+        state.items = [...state.items, _car];
+        state.subTotal = state.subTotal + _car.price;
       }
     },
     removeFromCart: (state, action) => {
-      let [..._car] = state.cart.filter((item) => item.id !== action.payload);
-      state.cart = [..._car];
-      state.total = state.cart.reduce((acc, curr) => {
+      let [..._car] = state.items.filter((item) => item.id !== action.payload);
+      state.items = [..._car];
+      state.subTotal = state.items.reduce((acc, curr) => {
         acc += curr.price * curr.quantity;
         return acc;
       }, 0);
     },
-    completeOrder: (state) => {
-      state.cart = [];
+    setTotal: (state, action) => {
+      state.tax = action.payload * .103;
+      state.total = state.subTotal * 1.103;
+    },
+    clearCart: (state) => {
+      state.items = [];
+      state.subTotal = 0;
+      state.tax = 0;
       state.total = 0;
     },
   },
 });
 
-export const { addToCart, removeFromCart, setTotal, setQuantity, completeOrder } = carSlice.actions;
+export const { addToCart, removeFromCart, setTotal, setQuantity, clearCart } = carSlice.actions;
 
 export default carSlice.reducer;
