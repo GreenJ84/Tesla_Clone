2f57ac34581911575fe38c8a9f404a17051dd3a6
Jesse Greenough <jesse.greenough84@gmail.com>
2023-05-26 23:23:48 -0700

    Migrate Order typings and state to seperate Redux slice
    Split Order form into seperate components mananaging their own form state
    
    Organize and Clean imports

--- src/pages/OrderPage.tsx
+++ src/pages/OrderPage.tsx
@@ -1,84 +1,28 @@
 /** @format */
 
-import { ChevronLeftIcon } from "@heroicons/react/24/solid";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import { OrderPageContainer } from "../app/Utils/StyledComponents/OrderComponents";
+import { ChevronLeftIcon } from "@heroicons/react/24/solid";
+
 import Header from "../components/Layout/Header";
 import Order1 from "../components/OrderPage/Order1";
 import Order2 from "../components/OrderPage/Order2";
+import SmallFooter from "../components/Layout/SmallFooter";
+import { OrderPageContainer } from "../app/Utils/StyledComponents/OrderComponents";
 
 import { useUserData } from "../app/Store/User/userSlice";
-import SmallFooter from "../components/Layout/SmallFooter";
-export interface Address {
-  firstName: string
-  lastName: string
-  address1: string
-  address2: string
-  zip: string
-  city: string
-  state: string
-}
-export interface shipAddress extends Address{
-  phone: string
-}
-export interface billAddress extends Address{
-  companyName: string
-  country: string
-}
+
 
 const OrderPage = () => {
   const nav = useNavigate();
   const user = useUserData();
 
-  const [shipAdd, setShipAdd] = useState<shipAddress>({
-    firstName: "",
-    lastName: "",
-    address1: "",
-    address2: "",
-    zip: "",
-    city: "",
-    state: "",
-    phone: ""
-  });
-  const [billAdd, setBillAdd] = useState<billAddress>({
-    companyName: "",
-    country: "",
-    firstName: "",
-    lastName: "",
-    address1: "",
-    address2: "",
-    zip: "",
-    city: "",
-    state: "",
-  });
-
-  const shipHandler = (value: keyof Address, e: React.ChangeEvent<HTMLInputElement>) => {
-    let shipCopy = {...shipAdd};
-    shipCopy[value] = e.currentTarget.value;
-    setShipAdd(shipAdd => ({...shipCopy}))
-  }
-  const billShipSame = () => {
-    let { phone, ...ship } = shipAdd;
-    setBillAdd(
-      {
-        ...ship,
-        companyName: "",
-        country: "USA"
-    })
-  }
-  const billHandler = (value: keyof billAddress, e: React.ChangeEvent<HTMLInputElement>) => {
-    let billCopy = {...billAdd};
-    billCopy[value] = e.currentTarget.value;
-    setBillAdd(billAdd => ({...billCopy}))
-  }
-
   const [secStep, setSecStep] = useState(false);
 
   useEffect(() => {
     if (!user.isLoggedIn) {
-      nav('/login')
+      nav("/login");
     }
   }, []);
 
@@ -86,29 +30,33 @@ const OrderPage = () => {
     <>
       <Header />
       <OrderPageContainer>
-        {secStep ? 
-          <>
-            <button onClick={() => setSecStep(false)}> <ChevronLeftIcon className="relative right-1 w-6 h-6"/>Back</button>
-            <p className="2xl:mb-4">Step 2 of 2</p>
-          </>
-        :
-          ""
-        }
-        {!secStep ?
+        {secStep && (
+          <button
+            aria-label="Return to previous step"
+            onClick={() => setSecStep(false)}
+          >
+            {" "}
+            <ChevronLeftIcon className="relative right-1 w-6 h-6" />
+            Back
+          </button>
+        )}
+        <p
+          aria-label="Checkout completion"
+          className="2xl:mb-4"
+        >
+          Step {secStep ? 2 : 1} of 2
+        </p>
+        {!secStep ? (
           <Order1
-            ship={[shipAdd, shipHandler]}
-            bill={[billAdd, billHandler, billShipSame]}
             setStep={() => setSecStep(true)}
           />
-          :
+        ) : (
           <Order2
-            ship={shipAdd}
-            bill={billAdd}
             setStep={() => setSecStep(false)}
           />
-        }
+        )}
       </OrderPageContainer>
-      <SmallFooter/>
+      <SmallFooter />
     </>
   );
 };
